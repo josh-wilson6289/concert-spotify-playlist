423feaf69fcfcf1f346126857886db3dfb551aa6
Josh Wilson <joshua.wilson6289@gmail.com>
2023-03-30 17:21:31 -0500

    installed tailwind css

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,8 +2,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
+    
     <Html lang="en">
       <Head />
+      <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
       <body>
         <Main />
         <NextScript />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import HomePage from './HomePage'
-
+import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,11 +14,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="main">
-        <div className="description">
-        Concert Spotify Playlist
+      <main>
         <HomePage />
-        </div>
+
       </main>
     </>
   )
